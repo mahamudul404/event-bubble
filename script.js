8148b007513c215954baeb14352766fc3dea93d9
Your New Name <mahamudul408432@gmail.com>
2024-10-06 13:17:11 +0600

    increase score 10

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let timer = 6;
+let score = 0;
+
+function increaseScore(){
+
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+
+  let hit = Math.floor(Math.random() * 10);
+
+  document.querySelector("#hitval").textContent = hit;
+}
+
 function makeBubble() {
   let clutter = "";
 
@@ -10,20 +26,20 @@ function makeBubble() {
   document.querySelector("#pbtm").innerHTML = clutter;
 }
 
-let timer = 6;
 function runTimer() {
   let timerInterval = setInterval(function () {
-    if(timer > 0) {
+    if (timer > 0) {
       timer--;
       document.querySelector("#timerval").textContent = timer;
-    }
-    else {
+    } else {
       clearInterval(timerInterval);
     }
-    
   }, 1000);
 }
 
 makeBubble();
 
 runTimer();
+
+getNewHit();
+
