7a8fa01424695c56850acc66b134ff65e6244c6e
Your New Name <mahamudul408432@gmail.com>
2024-10-06 13:01:37 +0600

    set game timer

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-let clutter = '';
+function makeBubble() {
+  let clutter = "";
 
-for(let i = 1; i<=75; i++){
+  for (let i = 1; i <= 75; i++) {
+    let rn = Math.floor(Math.random() * 10);
 
-  let rn = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble">${rn}</div>`;
+  }
 
-  clutter += `<div class="bubble">${rn}</div>`;
+  document.querySelector("#pbtm").innerHTML = clutter;
 }
 
-document.querySelector("#pbtm").innerHTML = clutter ;
+let timer = 6;
+function runTimer() {
+  let timerInterval = setInterval(function () {
+    if(timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    }
+    else {
+      clearInterval(timerInterval);
+    }
+    
+  }, 1000);
+}
+
+makeBubble();
+
+runTimer();
